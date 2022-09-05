07cce3aa2a1b8ee98a38269a37ff3b42e57c39d2
Gabriel Moraes <gmoraes.mat@gmail.com>
2022-09-05 15:00:16 -0300

    Implementando bônus

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express, { application } from 'express';
 import { MongoClient, ObjectId } from 'mongodb';
 import dotenv from 'dotenv';
-import joi from 'joi';
-import cors from 'cors';
 
+import cors from 'cors';
+import dayjs from 'dayjs';
 
 dotenv.config();
 
