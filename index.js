a436d9f2e7301a95c9cc39f154ee0b43a0ffd129
Gabriel Moraes <gmoraes.mat@gmail.com>
2022-09-05 17:14:02 -0300

    Bônus finalizado

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import joi from 'joi';
 import cors from 'cors';
 import dayjs from 'dayjs';
 
-dotenv.config();
+dotenv.config();  
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
